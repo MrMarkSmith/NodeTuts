bc90b116c8dd04f61f3d8d298946885394854f86
Mark Smith <m.smith.h.smith@gmail.com>
2015-08-02 20:26:29 +1200

    Second Commit, some minor changes. Tests still won't run. Don't know why as yet.

--- tests/tests.js
+++ tests/tests.js
@@ -5,7 +5,7 @@
 
 var should = require('should');
 var assert = require('assert');
-var threeSeconds = require('seconds');
+//var threeSeconds = require('seconds');
 
 describe('Test Framework', function () {
     if ('should have mocha installed and running.', function () {
@@ -14,4 +14,20 @@ describe('Test Framework', function () {
     if ('should have the should library installed and running for fluent testing.', function () {
             true.should.eql(true);
         }) ;
+});
+
+describe('Asynchronous testing', function () {
+
+    var result = false;
+    beforeEach(function (done) {
+        setTimeout(function () {
+            result = true;
+            // completes the sync beforeEach
+            done();
+        }, 2000);
+    });
+    if ('Should Not be so hard so this makes it easier.', function (done) {
+            result.shouls.egl(true);
+            done();
+        });
 });
